a518656fa851ffbd0f4e490dc4e02e926617b49b
fatiyaazhr <fatiyaazzahro@gmail.com>
2020-01-15 21:58:50 +0700

    second commit

---
+++ Arimatika.java
@@ -0,0 +1,21 @@
+public class Arimatika {
+     //Aritmatika perhitungan
+    public static void main(String[] args) {
+        // deklarasi variabel
+        int a,b;
+        double kali,bagi,tambah,kurang;
+        // memberikan nilai pada variabel      
+        a=50;
+        b=20; 
+        //menentukan rumus
+        tambah=a+a;
+        kurang=b-a;
+        kali=a*b;
+        bagi=b/a;
+        //Cetak nilai 
+        System.out.println("kali: "+kali);
+        System.out.println("bagi: "+bagi);
+        System.out.println("tambah: "+tambah);
+        System.out.println("kurang: "+kurang);
+    }   
+}
